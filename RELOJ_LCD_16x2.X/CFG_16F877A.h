e2ac7297a0bab60e07e41f4a06563c45584295ca
Hermes Colmenares <hermesjcc34@gmail.com>
2025-06-14 23:45:33 +0200

    Add all files repositori

---
+++ RELOJ_LCD_16x2.X/CFG_16F877A.h
@@ -0,0 +1,40 @@
+/* 
+ * File:   CFG_16F877A.h
+ * Author: Familia CS
+ *
+ * Created on 11 de abril de 2025, 21:40
+ */
+
+#ifndef CFG16F877A_H
+#define	CFG16F877A_H
+
+#pragma config FOSC = XT                                                        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF                                                       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON                                                       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON                                                       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF                                                        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF                                                        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF                                                        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF                                                         // Flash Program Memory Code Protection bit (Code protection off)
+
+#define _XTAL_FREQ 4000000
+
+#include <xc.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../RELOJ_LCD_16x2.X/dist/../debug/../../../Librerias_XC8.X/Display/LCD_16x2.X/CFG_LCD_16x2.h"
+#include "../../Librerias_XC8.X/I2C/I2C.X/CFG_I2C.h"
+
+#define SEL_OK          PORTBbits.RB4                                           //Tecla OK 
+#define DEC_2           PORTBbits.RB3                                           //Tecla hacia decremento
+#define INC_1           PORTBbits.RB2                                           //Tecla hacia incremento
+#define BEFORE_4        PORTBbits.RB1                                           //Tecla hacia izquierda
+#define NEXT_3          PORTBbits.RB0                                           //Tecla derecha
+
+#define DHT11           PORTBbits.RB5                                           //Dato de DHT11
+
+#define ALARMA          PORTCbits.RC2                                           //Salida alarma
+#define Led             PORTCbits.RC0                                           //Salida Leds
+
+#endif	/* CFG16F877A_H */
---
+++ RELOJ_LCD_16x2.X/RELOJ_LCD_16x2_MAIN.c
@@ -0,0 +1,45 @@
+/*
+ * File:   RELOJ_LCD_16x2_MAIN.c
+ * Author: Familia CS
+ *
+ * Created on 10 de abril de 2025, 00:00
+ * Asociado a Github
+ */
+
+#include "CFG_16F877A.h"
+//--------------------------------Interrupcion----------------------------------
+//------------------------------------------------------------------------------
+//---------------------------Inicio de CFG del micro----------------------------
+//------------------------------------------------------------------------------
+void main(void) 
+{
+    TRISA = 0;                                                                  //Puerto A RA0 como entrada               
+    TRISD = 0;                                                                  //Puerto D todos salidas
+    TRISE = 0;                                                                  //Puerto E todos salidas
+    TRISC = 0;                                                                  //PORTC7, PORTC1 y PORTC0 como entradas
+    TRISB = 0xFF;                                                               //Todos los pines de puerto B como entradas
+    CMCON = 0x07;                                                               //DESACTIVA LOS COMPARADORES
+    OPTION_REGbits.nRBPU = 0;                                                   //Activar push pull en puerto B
+    PORTA = 0; PORTB = 0; PORTC = 0; PORTD = 0; PORTE = 0;                      //Limpiar todos los puertos
+    CMCON = 0x07; ADCON1bits.PCFG = 6;                                          //DESACTIVA LOS COMPARADORES y Desactiva entradas analogicas
+//------------------------------------------------------------------------------    
+//---------------------------Programa de incio----------------------------------
+//------------------------------------------------------------------------------
+//****************************Inicializar LCD 4bits*****************************
+    Lcd_inicio_4bits();                                                         //Inicializar la LCD a 4bits
+//***************************Inicializar I2C************************************
+    I2C_Initialize();
+//*****************************Mensaje LCD inicio*******************************    
+    Lcd_pos_x(6);                                                               //Ubicar la LCD en posicion 6 de la coordenada X    
+    Lcd_Write_String("Reloj");                                                  //Escribir en la LCD "RELOJ"
+    Lcd_pos_y(4);                                                               //Ubicar en la posicion 4 de la coordenada Y
+    Lcd_Write_String("LCD 16x2");                                               //Escribir en la LCD "LCD 16x2"
+//******************************************************************************    
+    while (1)
+    {
+        Led = 1;
+        __delay_ms(100);
+        Led = 0;
+        __delay_ms(100);
+    }
+}
